540a92c29f57e48aff373b3fd9f8b2ea01952b83
Vaibhav Lachhwani <vaibhav.lachhwani111@gmail.com>
2023-10-05 21:15:44 +0530

    Make skeleton page

---
+++ timer.js
@@ -0,0 +1,33 @@
+(function() {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let dead = "Oct 5 2023 21:11:00",
+    countDown = new Date(dead).getTime(),
+    x = setInterval(function() {
+
+      let currentTime = new Date().getTime(),
+        distance = countDown - currentTime;
+
+      document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      if (distance < 0) {
+        let headline = document.getElementById("headline"),
+          countdown = document.getElementById("countdown"),
+          content = document.getElementById("content");
+
+        headline.innerText = "🎉Garba Gravity🎉";
+        countdown.style.display = "none";
+        content.style.display = "block";
+
+        clearInterval(x);
+      }
+      //seconds
+    }, 0)
+}());
